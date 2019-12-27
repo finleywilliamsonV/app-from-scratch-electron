d099aae342436cf4275f97cc213370a514ebf6f6
Finley Williamson V <finley.williamson@gmail.com>
2019-12-27 13:28:58 -0500

    Back to blank

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-console.log('starting main process js')
 const { app, BrowserWindow, ipcMain } = require('electron')
 const url = require('url')
 const path = require('path')
+
+console.log('starting main process js')
 
 let win
 
@@ -22,17 +23,5 @@ const createWindow = () => {
     // show dev tools
     win.webContents.openDevTools()
 }
-
-ipcMain.on('async-message', (e, args) => {
-    console.log('async message received by main process, args:', args)
-
-    e.sender.send('async-reply', 'async pong')
-})
-
-ipcMain.on('sync-message', (e, args) => {
-    console.log('sync message received by main process, args:', args)
-
-    e.returnValue = 'sync pong'
-})
 
 app.on('ready', createWindow)
--- renderer/index.js
+++ renderer/index.js
@@ -1,5 +1,7 @@
-console.log('starting renderer process js')
+const { ipcRenderer } = require('electron')
 const $ = require('jquery')
+
+console.log('starting renderer process js')
 
 let count = 0
 $('#click-counter').text(count.toString())
@@ -7,17 +9,3 @@ $('#countbtn').on('click', () => {
     count++
     $('#click-counter').text(count)
 })
-
-
-const { ipcRenderer } = require('electron')
-
-// Synchronous message emmiter and handler
-console.log(ipcRenderer.sendSync('sync-message', 'sync ping'))
-
-// Async message handler
-ipcRenderer.on('async-reply', (event, args) => {
-    console.log('async message received by renderer, args:', args)
-})
-
-// Async message sender
-ipcRenderer.send('async-message', 'async ping')
