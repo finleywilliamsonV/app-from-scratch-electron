9141cd5f536b051bd1c00ed2166e0f310ca5ac74
Finley Williamson V <finley.williamson@gmail.com>
2019-12-27 13:01:17 -0500

    Basic app with photon

---
+++ renderer/index.js
@@ -0,0 +1,9 @@
+console.log('starting renderer js')
+const $ = require('jquery')
+
+let count = 0
+$('#click-counter').text(count.toString())
+$('#countbtn').on('click', () => {
+    count++
+    $('#click-counter').text(count)
+})
