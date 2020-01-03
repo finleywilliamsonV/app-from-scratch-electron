9ee8f353f03d589c0fc2f1ae11ecdd24b093017e
Max <max.liao@gmail.com>
2020-01-03 10:15:41 -0500

    stash ffi

---
+++ renderer/ffi-lab/ffi-lab.js
@@ -0,0 +1,41 @@
+/* eslint-disable quote-props */
+const ffi = require('@saleae/ffi')
+const ref = require('@saleae/ref')
+const Struct = require('ref-struct')
+
+const { int } = ref.types
+
+/*
+    I CANNOT FOR THE LIFE OF ME FIGURE OUT HOW TO CONVERT THESE DINGUS DATA TYPES TO JS
+*/
+
+const TPCANMsg = Struct({
+    // eslint-disable-next-line quote-props
+    'ID': 'Uint32Array',
+    'MSGTYPE': '',
+    'LEN': '',
+    'DATA[8]': '',
+})
+/*
+
+
+typedef struct tagTPCANMsg
+{
+    DWORD             ID;      // 11/29-bit message identifier
+    TPCANMessageType  MSGTYPE; // Type of the message
+    BYTE              LEN;     // Data Length Code of the message (0..8)
+    BYTE              DATA[8]; // Data of the message (DATA[0]..DATA[7])
+} TPCANMsg;
+
+*/
+
+const TPCANMsgPtr = ref.refType(TPCANMsg)
+
+const canLibLoc = '../../PCANBasic.dll'
+
+const CAN = ffi.Library(canLibLoc, {
+    'CAN_Initialize': ['Uint32Array', ['Uint16Array', TPCANMsgPtr]]
+    // 'CAN_Write': ['Uint32Array', ['Uint16Array', TPCANMsgPtr]]
+})
+
+module.exports = CAN
--- renderer/index.js
+++ renderer/index.js
@@ -1,6 +1,7 @@
 const { ipcRenderer } = require('electron')
 const $ = require('jquery')
 const ffi = require('ffi')
+const ref = require('ref')
 
 $('#colorLabButton').on('click', () => {
     ipcRenderer.send('add-color-lab-window')
@@ -12,4 +13,16 @@ $('#jokeLabButton').on('click', () => {
 
 $('#ffi').on('click', () => {
     console.log('ffi', ffi)
+    console.log('ref.types', ref.types);
 })
+
+const myobj = ref.types.void // we don't know what the layout of "myobj" looks like
+const myobjPtr = ref.refType(myobj)
+
+let MyLibrary = ffi.Library('../pCAN/PCANBasic.dll', {
+  "do_some_number_fudging": [ 'double', [ 'double', 'int' ] ],
+//   "create_object": [ myobjPtr, [] ],
+//   "do_stuff_with_object": [ "double", [ myobjPtr ] ],
+//   "use_string_with_object": [ "void", [ myobjPtr, "string" ] ],
+//   "delete_object": [ "void", [ myobjPtr ] ]
+});
