e6ce0714122c2c9b41ebf956392dca8711714be4
Max <max.liao@gmail.com>
2020-01-03 15:20:13 -0500

    Linting

--- js/ffiComm.js
+++ js/ffiComm.js
@@ -3,59 +3,71 @@ const ref = require('ref')
 const Struct = require('ref-struct')
 const ArrayType = require('ref-array')
 
-const ulong = ref.types.ulong
-const ushort = ref.types.ushort
-const byte = ref.types.byte
+const { ulong } = ref.types
+const { ushort } = ref.types
+const { byte } = ref.types
+const { char } = ref.types
+
+const charPtr = ref.refType(char)
 
 const TPCANHandle = ushort
 const TPCANStatus = ulong
 const TPCANBaudrate = ushort
+const TPCANBitrateFD = charPtr
 
 const TPCANTimestamp = Struct({
     // DWORD  millis;             // Base-value: milliseconds: 0.. 2^32-1
     // WORD   millis_overflow;    // Roll-arounds of millis
     // WORD   micros;             // Microseconds: 0..999
-    'millis': ulong,
-    'millis_overflow': ushort,
-    'micros' : ushort
+    millis: ulong,
+    millis_overflow: ushort,
+    micros: ushort
 })
 
 const TPCANMsg = Struct({
-    'ID': ulong,
-    'MSGTYPE': byte,
-    'LEN': byte,
-    'DATA': ArrayType(byte,8)
+    ID: ulong,
+    MSGTYPE: byte,
+    LEN: byte,
+    DATA: ArrayType(byte, 8)
 })
-const TPCANMsgPtr = ref.refType(TPCANMsg)
+const TPCANMsgPtr = ref.refType(TPCANMsg);
 
-let PCANBasic = ffi.Library('./PCANBasic.dll', {
-  "CAN_Initialize": [ TPCANStatus, [ TPCANHandle, TPCANBaudrate, 'int', 'int', 'int' ] ],
-  'CAN_Read': [TPCANStatus, [TPCANHandle, TPCANMsgPtr, TPCANTimestamp ]],
-  'CAN_Write': [TPCANStatus, [TPCANHandle, TPCANMsg, TPCANTimestamp ]],
+const TPCANMsgFD = Struct({
+    ID: ulong,
+    MSGTYPE: byte,
+    LEN: byte,
+    DATA: ArrayType(byte, 8)
+})
+const TPCANMsgFDPtr = ref.refType(TPCANMsgFD)
 
-//   "create_object": [ myobjPtr, [] ],
-//   "do_stuff_with_object": [ "double", [ myobjPtr ] ],
-//   "use_string_with_object": [ "void", [ myobjPtr, "string" ] ],
-//   "delete_object": [ "void", [ myobjPtr ] ]
-});
+const PCANBasic = ffi.Library('./PCANBasic.dll', {
+    CAN_Initialize: [TPCANStatus, [TPCANHandle, TPCANBaudrate, 'int', 'int', 'int']],
+    CAN_InitializeFD: [TPCANStatus, [TPCANHandle, TPCANBitrateFD]],
+    CAN_Uninitialize: [TPCANStatus, [TPCANHandle]],
+    CAN_Reset: [TPCANStatus, [TPCANHandle]],
+    CAN_GetStatus: [TPCANStatus, [TPCANHandle]],
+    CAN_Read: [TPCANStatus, [TPCANHandle, TPCANMsgPtr, TPCANTimestamp]],
+    CAN_ReadFD: [TPCANStatus, [TPCANHandle, TPCANMsgFDPtr, TPCANTimestamp]],
+    CAN_Write: [TPCANStatus, [TPCANHandle, TPCANMsg, TPCANTimestamp]],
+})
 
 const PCAN_USBBUS1 = 81
 const PCAN_BAUD_50K = 18223
 PCANBasic.CAN_Initialize(PCAN_USBBUS1, PCAN_BAUD_50K, 0, 0, 0)
 
-let TPCANInit = new TPCANMsg({
-    'ID': 1536,
-    'MSGTYPE': 0,
-    'LEN': 1,
-    'DATA': [1, 0, 0, 0, 0, 0, 0, 0]
+const TPCANInit = new TPCANMsg({
+    ID: 1536,
+    MSGTYPE: 0,
+    LEN: 1,
+    DATA: [1, 0, 0, 0, 0, 0, 0, 0]
 })
 
 $('#PCANWrite').on('click', () => {
-    console.log(PCANBasic.CAN_Write(PCAN_USBBUS1, TPCANInit, 0));
+    console.log(PCANBasic.CAN_Write(PCAN_USBBUS1, TPCANInit, 0))
 })
 
 $('#PCANRead').on('click', () => {
     const emptyMessage = ref.alloc(TPCANMsg)
-    console.log(PCANBasic.CAN_Read(PCAN_USBBUS1, emptyMessage, 0));
-    console.log('READ', emptyMessage.deref());
+    console.log(PCANBasic.CAN_Read(PCAN_USBBUS1, emptyMessage, 0))
+    console.log('READ', emptyMessage.deref())
 })
