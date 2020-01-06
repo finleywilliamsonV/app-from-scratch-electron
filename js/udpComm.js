2bc48ac832b1d594c086ddada60a6733687f433d
Max <max.liao@gmail.com>
2020-01-06 10:46:24 -0500

    Linting udp

--- js/udpComm.js
+++ js/udpComm.js
@@ -1,105 +1,105 @@
-const uint16 = ref.types.uint16
-const uint32 = ref.types.uint32
+const { uint16 } = ref.types
+const { uint32 } = ref.types
 
-//Sean: This way makes a bit more sense than below but it does not bit-pack the bytes properly which is necessary for our protocol to function
+// Sean: This way makes a bit more sense than below but it does not bit-pack the bytes properly which is necessary for our protocol to function
 const DSCHeartbeat = Struct({
-  'Command' : uint16,
-  'ObjectID' : uint32,
-  'Status' : uint16,
-  'HBRate' : byte,
-  'FloorService': ArrayType(byte,32),
-  'Month' : byte,
-  'Day' : byte,
-  'Year' : byte,
-  'Hour' : byte,
-  'Minute' : byte,
-  'Second' : byte
+    Command: uint16,
+    ObjectID: uint32,
+    Status: uint16,
+    HBRate: byte,
+    FloorService: ArrayType(byte, 32),
+    Month: byte,
+    Day: byte,
+    Year: byte,
+    Hour: byte,
+    Minute: byte,
+    Second: byte
 })
 
-//Sean: I am not an advocate for this way, but it's the only way I can see it working unless we can get the above to bit-pack
+// Sean: I am not an advocate for this way, but it's the only way I can see it working unless we can get the above to bit-pack
 const DSCHeartbeatJSON = {
-    "Packet" : [
+    Packet: [
         {
-            "Command" : {
-                "Type" : "UInt16",
-                "Value" : 0x0000
+            Command: {
+                Type: 'UInt16',
+                Value: 0x0000
             }
         },
         {
-            "ObjectID" : {
-                "Type" : "UInt32",
-                "Value" : 0x01000000
+            ObjectID: {
+                Type: 'UInt32',
+                Value: 0x01000000
             }
         },
         {
-            "Status" : {
-                "Type" : "UInt16",
-                "Value" : 0x0800
+            Status: {
+                Type: 'UInt16',
+                Value: 0x0800
             }
         },
         {
-            "HBRate" : {
-                "Type" : "Byte",
-                "Value" : 5
+            HBRate: {
+                Type: 'Byte',
+                Value: 5
             }
         },
         {
-            "FloorService" : {
-                "Type" : "Array",
-                "Value" : Array(32).fill(0xFF)
+            FloorService: {
+                Type: 'Array',
+                Value: Array(32).fill(0xFF)
             }
         },
         {
-            "Month" : {
-                "Type" : "Byte",
-                "Value" : 1
+            Month: {
+                Type: 'Byte',
+                Value: 1
             }
         },
         {
-            "Day" : {
-                "Type" : "Byte",
-                "Value" : 3
+            Day: {
+                Type: 'Byte',
+                Value: 3
             }
         },
         {
-            "Year" : {
-                "Type" : "Byte",
-                "Value" : 50 //since 1970
+            Year: {
+                Type: 'Byte',
+                Value: 50 // since 1970
             }
         },
         {
-            "Hour" : {
-                "Type" : "Byte",
-                "Value" : 15
+            Hour: {
+                Type: 'Byte',
+                Value: 15
             }
         },
         {
-            "Minute" : {
-                "Type" : "Byte",
-                "Value" : 37
+            Minute: {
+                Type: 'Byte',
+                Value: 37
             }
         },
         {
-            "Second" : {
-                "Type" : "Byte",
-                "Value": 0
+            Second: {
+                Type: 'Byte',
+                Value: 0
             }
         }
     ]
 }
 
-//should work for any kiosk on Group 1
-const PORT = 7039;
-const HOST = "192.168.0.255";
+// should work for any kiosk on Group 1
+const PORT = 7039
+const HOST = '192.168.0.255'
 
-const dgram = require('dgram');
+const dgram = require('dgram')
 
-//let UDPtestMsg = Buffer.alloc('My KungFu is Good!'.length,'My KungFu is Good!');
+// let UDPtestMsg = Buffer.alloc('My KungFu is Good!'.length,'My KungFu is Good!');
 
 // This uses the JSON structure to "bit-pack" manually
-let heartbeatBuffer = []
+const heartbeatBuffer = []
 DSCHeartbeatJSON["Packet"].forEach(params => {
-    for (var param in params )
+    for (let param in params )
     {
         let item = params[param]
         if ( item["Type"] == "Byte" )
@@ -130,7 +130,7 @@ DSCHeartbeatJSON["Packet"].forEach(params => {
 
 let UDPtestMsg = Buffer.from(heartbeatBuffer)
 
-var client = dgram.createSocket('udp4');
+const client = dgram.createSocket('udp4')
 client.bind(PORT)
 
 // let server = dgram.createSocket('udp4');
@@ -145,9 +145,9 @@ client.bind(PORT)
 // server.bind(PORT, HOST);
 
 $('#UDPWrite').on('click', () => {
-    client.send(UDPtestMsg, 0, UDPtestMsg.length, PORT, HOST, function(err, bytes) {
-        if (err) throw err;
-        console.log('UDP message sent to ' + HOST +':'+ PORT);
-        //client.close() //don't close the client
-    });
+    client.send(UDPtestMsg, 0, UDPtestMsg.length, PORT, HOST, (err, bytes) => {
+        if (err) throw err
+        console.log(`UDP message sent to ${HOST}:${PORT}`)
+        // client.close() //don't close the client
+    })
 })
