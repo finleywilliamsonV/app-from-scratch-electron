bee5de8933d5fba76f194ea8e7f2a4598cfb3130
Finley Williamson V <finley.williamson@gmail.com>
2020-01-06 15:34:07 -0500

    Cleanup residual code

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const { app, shell, ipcMain, net } = require('electron')
 const contextMenu = require('electron-context-menu')
 const path = require('path')
 const Window = require('./js/Window')
-const isJokeClean = require('./renderer/joke-lab/joke-filter')
 
 const main = () => {
     const mainWindow = new Window({
@@ -15,85 +14,6 @@ const main = () => {
     })
 
     mainWindow.maximize()
-
-    // -----  COLOR LAB  -----
-    let colorLabWindow
-
-    // create color lab window
-    ipcMain.on('add-color-lab-window', () => {
-        if (!colorLabWindow) {
-            colorLabWindow = new Window({
-                file: path.join('renderer', 'color-lab', 'color-lab.html'),
-                width: 400,
-                height: 300,
-                webPreferences: {
-                    nodeIntegration: true
-                },
-                parent: mainWindow
-            })
-
-            colorLabWindow.on('closed', () => {
-                colorLabWindow = null
-            })
-        }
-    })
-
-    // -----  JOKE LAB  -----
-    let jokeLabWindow
-
-    // create color lab window
-    ipcMain.on('add-joke-lab-window', () => {
-        if (!jokeLabWindow) {
-            jokeLabWindow = new Window({
-                file: path.join('renderer', 'joke-lab', 'joke-lab.html'),
-                width: 400,
-                height: 600,
-                webPreferences: {
-                    nodeIntegration: true
-                },
-                parent: mainWindow
-            })
-
-            jokeLabWindow.on('closed', () => {
-                jokeLabWindow = null
-            })
-        }
-    })
-
-    // make http request
-    ipcMain.on('http-request', (e, args) => {
-        // function to make the request
-        const makeRequest = (url) => {
-            const request = net.request(url)
-            request.on('response', (res) => {
-                if (res.statusCode === 200) {
-                    res.on('data', (chunk) => {
-                        const jokeJSON = JSON.parse(chunk)
-
-                        if (isJokeClean(jokeJSON)) {
-                            // return the data
-                            e.sender.send('http-response', jokeJSON)
-                        } else {
-                            // make another request
-                            makeRequest(url)
-                        }
-                    })
-                } else if (res.statusCode === 429) {
-                    e.sender.send('http-response', {
-                        type: 'twopart',
-                        setup: 'We\'ve made too many requests.',
-                        delivery: 'This is not a joke. @___@'
-                    })
-                }
-            })
-            // finalize the request to send it
-            request.end()
-        }
-        // make the request
-        if (args.url) {
-            makeRequest(args.url)
-        }
-    })
 }
 
 // create/configure the context (right-click) menu
--- renderer/index.js
+++ renderer/index.js
@@ -1,10 +1,2 @@
 const { ipcRenderer } = require('electron')
 const $ = require('jquery')
-
-$('#colorLabButton').on('click', () => {
-    ipcRenderer.send('add-color-lab-window')
-})
-
-$('#jokeLabButton').on('click', () => {
-    ipcRenderer.send('add-joke-lab-window')
-})
